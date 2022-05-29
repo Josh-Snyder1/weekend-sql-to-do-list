933a7a2c569b8ef7fcb138e7f255a262488b2975
Josh Snyder <joshrsnyder@gmail.com>
2022-05-29 16:10:10 -0500

    structure setup complete

---
+++ server/server.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const toDoRouter = require('./routes/toDo.router');
+
+const app = express();
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use('/toDo');
+
+app.use(express.static('server/public'));
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+    console.log('listening on PORT', PORT);
+});
