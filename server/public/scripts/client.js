002675b421fd3c7f2b4365b94e9ab66b20a2d965
Josh Snyder <joshrsnyder@gmail.com>
2022-05-29 17:39:36 -0500

    properly takes new task and post to database

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -4,9 +4,31 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log( 'so ready!' );
-    addTask();
+    $(document).on('click', '#addButton', addTask);
 };
 
 function addTask() {
-    
-}
+
+    let task = {
+        task: $('#taskIn').val()
+    };
+    console.log('add button clicked',task);
+
+    $.ajax({
+        type: 'POST',
+        url: '/toDo',
+        data: task
+    }).then(function(response) {
+        getTasks();
+        console.log('in ajax post.then',response);
+    }).catch(function(err) {
+        console.log('error in POST', err);
+        alert('unable to add task');
+    })
+
+    $('#taskIn').val('');
+}// end add task
+
+function appendTasks() {
+    console.log('in appendTasks');
+}// end appendTasks
